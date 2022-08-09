279badf9feb79fa9ac4fada3b015bdbc885689fd
Nick's Scripts <95194821+NickScripts@users.noreply.github.com>
2022-08-08 21:15:41 -0500

    Add files via upload

---
+++ ns_bikerental/config.lua
@@ -0,0 +1,40 @@
+Config = {}
+Config.Locale = 'en'
+--Main Shit--
+Config.EnablePrice = true
+Config.EnableEffects = true
+Config.EnableBlips = true
+--Bike Prices--
+Config.PriceTriBike = 69
+Config.PriceScorcher = 99
+Config.PriceCruiser = 129
+Config.PriceBmx = 109
+--Markers n shi--
+Config.TypeMarker = 27
+Config.MarkerScale = { x = 2.000, y = 2.000, z = 0.500}
+Config.MarkerColor = { r = 0, g = 255, b = 255}
+Config.MarkerZones = { 
+    vector3(-135.042, -807.760, 31.838 - 0.95),
+    vector3(-597.811, -336.159, 34.863 - 0.95),
+    vector3(127.9686, -1025.11, 29.357 - 0.95),
+    vector3(1184.080, -417.741, 67.510 - 0.95),
+    vector3(434.3549, 129.3740, 100.33 - 0.95),
+    vector3(-1524.30, -459.881, 35.419 - 0.95),
+    vector3(1809.047, 3676.576, 34.276 - 0.95),
+    vector3(1937.770, 3723.122, 32.870 - 0.95),
+    vector3(-230.320, 6291.562, 31.451 - 0.95),
+    vector3(1681.560, 4828.743, 42.066 - 0.95),
+}
+--Blips or wtv--
+Config.BlipZones = { 
+   {title="Bike Rental", colour=2, id=376, x = -135.042, y = -807.760, z = 31.838},
+   {title="Bike Rental", colour=2, id=376, x = -597.811, y = -336.159, z = 34.863},
+   {title="Bike Rental", colour=2, id=376, x = 127.9686, y = -1025.11, z = 29.357},
+   {title="Bike Rental", colour=2, id=376, x = 1184.080, y = -417.741, z = 67.510},
+   {title="Bike Rental", colour=2, id=376, x = 434.3549, y = 129.3740, z = 100.33},
+   {title="Bike Rental", colour=2, id=376, x = -1524.30, y = -459.881, z = 35.419},
+   {title="Bike Rental", colour=2, id=376, x = 1809.047, y = 3676.576, z = 34.276},
+   {title="Bike Rental", colour=2, id=376, x = 1937.770, y = 3723.122, z = 32.870},
+   {title="Bike Rental", colour=2, id=376, x = -230.320, y = 6291.562, z = 31.451},
+   {title="Bike Rental", colour=2, id=376, x = 1681.560, y = 4828.743, z = 42.066},
+}
---
+++ ns_bikerental/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Nicks Scripts'
+
+client_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'config.lua',
+  'client/main.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
+}	
+
+lua54 'yes'
